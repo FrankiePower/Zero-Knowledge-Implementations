842e0274207f47f7980571f6d542ce6269d0279b
frankie-powers <franklin.power21@gmail.com>
2025-02-10 07:10:09 +0100

    updated the code to use hashmaps and and a custom pow function.

--- univariate_polynomial_implementation/implementation/src/main.rs
+++ univariate_polynomial_implementation/implementation/src/main.rs
@@ -2,60 +2,102 @@
 // implementation with dense rep
 // use generics too
 // evaluate with for loop and without
+// interpolation
 
-struct PolynomialTerm {
-    coefficient: usize,
-    degree: usize,
+use std::collections::HashMap;
+use std::io;
+use std::ops::{Add, Div, Mul, Sub};
+
+// Sparse Representation: Only stores the non-zero terms
+struct SparsePolynomial<T> {
+    terms: HashMap<usize, T>, // degree -> coefficient
+}
+
+impl<T: Copy + Add<Output = T> + Mul<Output = T> + Default + From<u8>> SparsePolynomial<T> {
+    fn evaluate(&self, x: T) -> T {
+        let mut result = T::default();
+
+        for (degree, coefficient) in &self.terms {
+            result = result + *coefficient * pow(x, *degree);
+        }
+
+        result
+    }
+}
+
+fn pow<T: Copy + Mul<Output = T> + From<u8>>(base: T, exp: usize) -> T {
+    let mut result = T::from(1);
+    for _ in 0..exp {
+        result = result * base;
+    }
+    result
+}
+
+// // Dense Representation: Stores all coefficients, including zeros
+struct DensePolynomial<T> {
+    coefficients: Vec<T>,
+}
+
+impl<T: Copy + Add<Output = T> + Mul<Output = T> + Default + From<u8>> DensePolynomial<T> {
+    fn evaluate(&self, x: T) -> T {
+        self.coefficients
+            .iter()
+            .enumerate()
+            .fold(T::default(), |acc, (i, &coeff)| {
+                acc + (coeff * pow(x, i)) // Correct order
+            })
+    }
+}
+
+// Lagrange Interpolation: Given points (x_i, y_i), find f(x)
+fn lagrange_interpolation<T>(points: &[(T, T)], x: T) -> T
+where
+    T: Copy
+        + Default
+        + Add<Output = T>
+        + Mul<Output = T>
+        + Mul<Output = T>
+        + Sub<Output = T>
+        + Div<Output = T>,
+{
+    let mut result = T::default();
+    for (i, &(xi, yi)) in points.iter().enumerate() {
+        let mut term = yi;
+        for (j, &(xj, _)) in points.iter().enumerate() {
+            if i != j {
+                term = term * ((x - xj) / (xi - xj));
+            }
+        }
+        result = result + term;
+    }
+    result
 }
 
 fn main() {
-    let poly = PolynomialTerm {
-        coefficient: 3,
-        degree: 2,
-    }; // 3x^2
+    // Example: 3x^2 + 2x + 5 in both representations
+    let sparse_poly = SparsePolynomial {
+        terms: [(2, 3), (1, 2), (0, 5)].iter().cloned().collect(),
+    };
 
-    // let poly = Monomial::new(3, 2);
+    let dense_poly = DensePolynomial {
+        coefficients: vec![5, 2, 3],
+    };
 
-    let result = poly.evaluate(5);
+    println!("Please input the value of x");
 
-    println!("The polynomial evaluated at x = 5 is: {}", result);
+    let mut input = String::new();
 
-    println!("The degree of the polynomial is: {}", poly.degree());
-}
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
 
-fn evaluate(self, x: usize) -> usize {
-    // evaluate coefficient * x^degree
-    self.coefficient * x.pow(self.degree as u32)
-}
+    let x: usize = input.trim().parse().unwrap();
 
-impl PolynomialTerm {
-    fn degree(self) -> usize {
-        self.degree
-    }
-
-    fn evaluate(self, x: usize) -> usize {
-        self.coefficient * x.pow(self.degree as u32)
-    }
+    println!("Sparse polynomial evaluates to:{}", sparse_poly.evaluate(x));
+    println!("Dense polynomial evaluates to:{}", dense_poly.evaluate(x));
 }
 
 /*
-
-struct UnivariatePoly {
-    // 1 coeffiecient for each power of x
-    coefficient: Vec<f64>,
-}
-
-impl UnivariatePoly {
-    fn degree(&self) -> usize {
-        self.coefficient.len() - 1
-    }
-
-    fn evaluate(&self, x: f64) -> f64 [
-
-    ]
-}
-
-    // Dense Representation of Polynomials
 
 // 2 functions (evaluate and interpolate)
 
@@ -65,9 +107,5 @@ impl UnivariatePoly {
 
 // how do we represent 5 , 2x + 5 , 3x^2 + 2x + 5
 
-struct Monomial {
-    coefficient: f64,
-    degree: usize,
-}
 
  */
